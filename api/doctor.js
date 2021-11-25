bdf1828e3e15ebde741a500a616e8157d1329c89
kumol <kumol01763@gmail.com>
2021-11-26 00:58:50 +0600

    Get all doctor

--- api/doctor.js
+++ api/doctor.js
@@ -4,12 +4,21 @@ const Doctor = require("../models/doctor");
 const doctorsHelper = require("./controller/doctorController");
 var router = express.Router();
 
-router.get("/all",(req,res)=>{
-    Doctor.find({}).then((result)=>{
-        res.status(200).json({result});
-    }).catch((err)=>{
-        res.status(500).json({error:err});
-    })
+router.get("/all",async(req,res)=>{
+    try{
+        let limit = req.query.limit && Number(req.query.limit) > 0? Number(req.query.limit) : 10;
+        let page = req.query.page && Number(req.query.page)>=0 ? Number(req.query.page) : 1;
+        let doctor = page == 0 || limit == 0 ? [] : await Doctor.find({}).skip(limit*(page-1)).limit(limit);
+        let total = await Doctor.countDocuments();
+        return Ok(res, "Request Successful", {
+            data: doctor,
+            total: total,
+            page: page,
+            limit: limit
+        });
+    }catch(error){
+        return ErrorResponse(res, error.message, error)
+    }
 });
 
 router.post("/save", async(req,res,next)=>{
@@ -18,9 +27,9 @@ router.post("/save", async(req,res,next)=>{
         newDoctor.experience = newDoctor.experience ? newDoctor.experience.length ? newDoctor.experience : [newDoctor.experience] : [];
         newDoctor.degrees = newDoctor.degrees ? newDoctor.degrees.length ? newDoctor.degrees : [newDoctor.degrees] : [];
         let doctor = new Doctor(newDoctor);
-        doctor.fullName = doctor.firstName + doctor.lastName;
+        doctor.fullName = doctor.firstName + " " + doctor.lastName;
         doctor.id = doctor._id;
-        let doctor = Doctor.create(doctor);
+        doctor = await Doctor.create(doctor);
 
         return Ok(res, "Request Successful", doctor);
     }catch(error){
@@ -28,16 +37,15 @@ router.post("/save", async(req,res,next)=>{
     }
 });
 
-router.get("/:id",doctorsHelper.getDoctorById);
+router.get("/single/:id",doctorsHelper.getDoctorById);
 
-router.delete("/delete/:id",(req,res,next)=>{
-    Doctor.remove({_id:req.params.id}).then((result)=>{
-        res.status(200).json({result});
-    }).catch((err)=>{
-        res.status(500).json({
-            error:err
-        })
-    })
+router.delete("/delete/:id", async(req,res,next)=>{
+    try{
+        let deleteOne = await Doctor.deleteOne({_id:req.params.id})
+        return Ok(res, "Request Successful", {"msg": "Object deleted"});
+    }catch(error){
+        return ErrorResponse(res, error.message, error);
+    }
 })
 
 router.get("/organization/:id",doctorsHelper.getDoctorByOrganizationId);
