0180ff329d20f8823b516151178e82397ceb6733
yotechltd <91316425+yotechltd@users.noreply.github.com>
2021-11-25 18:06:29 +0600

    Doctor schema structure updated

--- models/doctor.js
+++ models/doctor.js
@@ -1,18 +1,43 @@
 const mongoose = require("mongoose");
 const doctorSchema = mongoose.Schema({
-    name: { type: String },
+        name: { type: String },
     bname: { type: String },
     doctorId: { type: String },
     address: { type: String },
     college: { type: String },
     email: { type: String },
     contactNumber: { type: String },
-    degrees: [{ type: mongoose.Schema.Types.ObjectId, ref: "Degree" }],
-    chamber: [{ type: mongoose.Schema.Types.ObjectId, ref: "Chamber" }],
+    degrees: [
+        {
+            title: String,
+            details: String,
+            institute: String,
+            country: String,
+            topic: String,
+        }
+    ],
+    chamber: [{ 
+        startDay: String,
+        endDay: String,
+        dayRange: [{ type: String }],
+        timeRange: String,
+        chamberId: {type: mongoose.Schema.Types.ObjectId, ref: "Chamber"},
+        numberOfAppointment: Number,
+        appointment: [{type: mongoose.Schema.Types.ObjectId, ref: "Appointment"}]
+    }],
     image: { type: String },
     details: { type: String },
-    experience:[{ type: mongoose.Schema.Types.ObjectId, ref: "Experience" }],
-    skill: [ { type: mongoose.Schema.Types.ObjectId, ref: "Skill" }]
+    experience:[
+        {
+            position: String,
+            organization: String,
+            startDate: Date,
+            endDate: Date,
+            days: Number,
+            workingPeriod: String,
+            running: Boolean,
+        }
+    ]
 });
 
 module.exports = mongoose.model("Doctor", doctorSchema);
