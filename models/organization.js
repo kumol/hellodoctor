3ec7778430d95e7cf43f44774230a13a3fe8b505
yotechltd <91316425+yotechltd@users.noreply.github.com>
2021-11-25 18:04:35 +0600

    Organization schema changed

--- models/organization.js
+++ models/organization.js
@@ -9,13 +9,26 @@ let organizationSchema = mongoose.Schema({
     email: { type: String },
     website: { type: String },
     address: { type: String },
-    district: { type: mongoose.Schema.Types.ObjectId, ref: "District" },
-    division: { type: mongoose.Schema.Types.ObjectId, ref: "Division" },
+    location: {
+        type: {
+          type: String, // Don't do `{ location: { type: String } }`
+          enum: ['Point'], // 'location.type' must be 'Point'
+          required: true,
+          default: 'Point'
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },
+    town: { type: mongoose.Schema.Types.ObjectId, ref: "Town" },
+    city: { type: mongoose.Schema.Types.ObjectId, ref: "City" },
     logo: { type: String },
     appointment: [{ type: mongoose.Schema.Types.ObjectId, ref: "Appointment" }],
     startTime:Date,
     endTime:Date,
-    doctor: [{ ref: "Doctor", type: mongoose.Schema.Types.ObjectId }]
+    doctor: [{ ref: "Doctor", type: mongoose.Schema.Types.ObjectId }],
+    chamber: [{ type: mongoose.Schema.Types.ObjectId , ref: "Chamber"}]
 });
 
 module.exports = mongoose.model("Organization", organizationSchema);
