ca3c4707f7253550b431bd1ce45d8034c2a7d42c
kumol <kumol01763@gmail.com>
2021-12-02 00:21:17 +0600

    user schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
-    name: { type: String, default: ""},
+    name: { type: String, default: "", required: true},
     id: { type: String},
+    password: { type: String, required: true},
+    city: String,
+    location: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: true,
+          default: 'Point'
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },
     number: { type: String , default: ""},
     accountType: { type:Number , default: 1, enum: [1,2,3]},
     email: { type: String , default: ""},
